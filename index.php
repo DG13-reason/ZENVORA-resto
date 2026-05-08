877b563912c4e829d1b064ca950dae38d8ec5672
DG13-Reason <damargiri13@gmail.com>
2026-05-09 02:27:48 +0800

    menambah searching pada index dan membagikan styling dengan tag komentar pada css

--- index.php
+++ index.php
@@ -2,5 +2,24 @@
 
 <?php include 'includes/navbar.php'; ?>
 
-<h1>HELLOW WORDL</h1>
+
+<section>
+    <div class="search-menu">
+        <input type="text" placeholder="searching menu">
+        <button>
+            <i data-feather="search"></i>
+        </button>
+    </div>
+</section>
+
+<section class="new-info">
+    <div class="info-banner">
+        
+    </div>
+</section>
+
+<section class="menu-section">
+
+</section>
+
 <?php include 'includes/footer.php'; ?>
