652ed6c08d3470f8bdb54d14d580e7255a9b255e
DG13-Reason <damargiri13@gmail.com>
2026-05-12 23:31:03 +0800

    menambah bagian section pada index

--- index.php
+++ index.php
@@ -39,7 +39,147 @@
 
 
 <section class="menu-section">
-
+    <div class="menu-tittleSect">
+        <h2>Recommendation Menu</h2>
+    </div>
+    <div class="menu-reco">
+        <div class="main-course">
+            <h3>Main Course</h3>
+        </div>
+        <div class="Recomend-container">
+            <div class="menu-card">
+                <img src="assets/images/Menu/Bakso.png" alt="">
+                <div class="menu-desc">
+                    <h4>Bakso</h4>
+                    <p>Bakso dengan cita rasa pedasnya</p>
+                    <div class="menu-price">
+                        <span class="price">Rp20.000</span>
+                        <span class="rating">
+                            <i data-feather="star"></i>4.5
+                        </span>
+                    </div>
+                    <div class="button-price">
+                        <a href="">
+                            <i data-feather="shopping-cart"></i>
+                        </a>
+                        <button>Pesan</button>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-card">
+                <img src="assets/images/Menu/Nasgor.png" alt="">
+                <div class="menu-desc">
+                    <h4>Nasi Goreng</h4>
+                    <p>Bakso dengan cita rasa pedas & manis</p>
+                    <div class="menu-price">
+                        <span class="price">Rp25.000</span>
+                        <span class="rating">
+                            <i data-feather="star"></i>4.5
+                        </span>
+                    </div>
+                    <div class="button-price">
+                        <a href="">
+                            <i data-feather="shopping-cart"></i>
+                        </a>
+                        <button>Pesan</button>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-card">
+                <img src="assets/images/Menu/MieAyam.png" alt="">
+                <div class="menu-desc">
+                    <h4>Mie Ayam</h4>
+                    <p>Bakso dengan cita rasa pedasnya</p>
+                    <div class="menu-price">
+                        <span class="price">Rp16.000</span>
+                        <span class="rating">
+                            <i data-feather="star"></i>4.5
+                        </span>
+                    </div>
+                    <div class="button-price">
+                        <a href="">
+                            <i data-feather="shopping-cart"></i>
+                        </a>
+                        <button>Pesan</button>
+                    </div>
+                </div>
+            </div>
+            
+        
+        <div class="main-course">
+            <a href="#">View All</a>
+        </div>
+    </div>
+</section>
+<section class="menu-section">
+    <div class="menu-reco">
+        <div class="main-course">
+            <h3>Drinks</h3>
+        </div>
+        <div class="Recomend-container">
+            <div class="menu-card">
+                <img src="assets/images/Menu/JusAlpukat.png" alt="">
+                <div class="menu-desc">
+                    <h4>Jus Alpukat</h4>
+                    <p>Jus Alpukat dengan rasa segar dan aromanya yang kuat</p>
+                    <div class="menu-price">
+                        <span class="price">Rp10.000</span>
+                        <span class="rating">
+                            <i data-feather="star"></i>4.5
+                        </span>
+                    </div>
+                    <div class="button-price">
+                        <a href="">
+                            <i data-feather="shopping-cart"></i>
+                        </a>
+                        <button>Pesan</button>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-card">
+                <img src="assets/images/Menu/EsTeler.png" alt="">
+                <div class="menu-desc">
+                    <h4>Es Teler</h4>
+                    <p>Es teler dengan buah dan cita rasanya yang manis dan segar</p>
+                    <div class="menu-price">
+                        <span class="price">Rp15.000</span>
+                        <span class="rating">
+                            <i data-feather="star"></i>4.5
+                        </span>
+                    </div>
+                    <div class="button-price">
+                        <a href="">
+                            <i data-feather="shopping-cart"></i>
+                        </a>
+                        <button>Pesan</button>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-card">
+                <img src="assets/images/Menu/Matcha.png" alt="">
+                <div class="menu-desc">
+                    <h4>Matcha Latte</h4>
+                    <p>Matcha latte perpaduan antara matcha dan kopi yang pahit</p>
+                    <div class="menu-price">
+                        <span class="price">Rp17.000</span>
+                        <span class="rating">
+                            <i data-feather="star"></i>4.5
+                        </span>
+                    </div>
+                    <div class="button-price">
+                        <a href="">
+                            <i data-feather="shopping-cart"></i>
+                        </a>
+                        <button>Pesan</button>
+                    </div>
+                </div>
+            </div>
+            
+        
+        <div class="main-course">
+            <a href="#">View All</a>
+        </div>
+    </div>
 </section>
 
 <?php include 'includes/footer.php'; ?>
