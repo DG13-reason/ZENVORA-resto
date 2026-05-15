003924ad4e0cb2c257e042fe481565edc5290f2b
Nisa <hairunnisa0519@gmail.com>
2026-05-15 14:42:20 +0800

    halaman about

---
+++ about.php
@@ -0,0 +1,81 @@
+<?php include 'includes/header.php'; ?>
+
+<?php include 'includes/navbar.php'; ?>
+
+<section class="tentang">
+
+    <div class="text">
+        <button class="judul">TENTANG KAMI</button>
+
+        <h1>TENTANG <span>ZENVORA</span></h1>
+
+        <p>
+            ZENVORA resto adalah platform digital yang menggabungkan
+            layanan pemesanan makanan dan pencarian lowongan kerja.
+        </p>
+
+        <p>
+            Melalui ZENVORA, Anda dapat menemukan berbagai makanan
+            favorit dan peluang karier terbaik.
+        </p>
+
+        <button class="btn">Pelajari lebih lanjut</button>
+
+        <div class="sosmed">
+            <img src="instagram.png">
+            <img src="gmail.png">
+            <img src="wa.png">
+        </div>
+    </div>
+
+    <div class="gambar">
+        <img src="laptop.png" alt="">
+    </div>
+
+</section>
+
+<section class="box">
+
+    <div class="visi">
+        <h2>Visi Kami</h2>
+        <p>
+            Menjadi platform digital terdepan
+            dalam kebutuhan makanan dan karier.
+        </p>
+    </div>
+
+    <div class="misi">
+        <h2>Misi Kami</h2>
+        <p>
+            Menyediakan layanan praktis,
+            cepat, dan terpercaya.
+        </p>
+    </div>
+
+</section>
+
+<section class="keunggulan">
+
+    <h2>Keunggulan ZENVORA</h2>
+
+    <div class="icon-box">
+        <div>
+            <h3>Mudah Digunakan</h3>
+        </div>
+
+        <div>
+            <h3>Aman & Terpercaya</h3>
+        </div>
+
+        <div>
+            <h3>Cepat & Praktis</h3>
+        </div>
+
+        <div>
+            <h3>Layanan Terbaik</h3>
+        </div>
+    </div>
+
+</section>
+
+<?php include 'includes/footer.php'; ?>
