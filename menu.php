0c4e152eb11e001b691336534d55396da60d23ca
febri <febybima32@gmail.com>
2026-06-02 14:22:41 +0800

    memperbaiki menu

--- menu.php
+++ menu.php
@@ -1,134 +1,265 @@
+<?php
+session_start();
 
+// TAMBAH KE KERANJANG
+if(isset($_POST['cart'])){
+
+    $id = $_POST['id'];
+
+    if(isset($_SESSION['cart'][$id])){
+        $_SESSION['cart'][$id]['qty']++;
+    }else{
+        $_SESSION['cart'][$id] = [
+            'nama'  => $_POST['nama'],
+            'harga' => $_POST['harga'],
+            'qty'   => 1
+        ];
+    }
+
+    header("Location: cart.php");
+    exit;
+}
 
 include 'includes/header.php';
 include 'includes/navbar.php';
 
+// DATA MAKANAN
+$makanan = [
+    ["Nasi Goreng",20000,"assets/images/nasigoreng.png"],
+    ["Bakso",15000,"assets/images/bakso.png"],
+    ["Mie Ayam",15000,"assets/images/mieayam.png"],
+    ["Nila Bakar",25000,"assets/images/nilabakar.jpg"],
+    ["Rawon",25000,"assets/images/rawon.jpg"],
+    ["Sate Lilit",15000,"assets/images/satelilit.jpg"],
+    ["Seafood Mix",35000,"assets/images/seafoodmix.jpg"],
+    ["Steak",55000,"assets/images/steak.jpg"]
+];
+
+// DATA SNACK
+$snack = [
+    ["French Fries",12000,"assets/images/kentang.png"],
+    ["Risol Mayo",15000,"assets/images/Risol.png"],
+    ["Sandwich",15000,"assets/images/sandwich.jpg"],
+    ["Pancake",12000,"assets/images/pancake.jpg"]
+];
+
+// DATA MINUMAN
+$minuman = [
+    ["Jus Alpukat",12000,"assets/images/jusalpukat.png"],
+    ["Kopi",10000,"assets/images/kopi.jpg"],
+    ["Boba Matcha",15000,"assets/images/matcha.png"],
+    ["Boba Taro",15000,"assets/images/bobataro.jpg"]
+];
+?>
+
 <section class="menu-section">
 
     <div class="menu-search">
-        <input type="text" placeholder="searching">
+        <input type="text" placeholder="Cari menu...">
     </div>
 
-    <h2 class="menu-title">MAKANAN</h2>
+    <h2 class="menu-title">MENU RESTORAN</h2>
+
     <p class="menu-subtitle">
         Pilih makanan favoritmu dari berbagai pilihan lezat kami.
     </p>
 
     <div class="menu-kategori">
-        <button class="active">Semua</button>
-        <button>Makanan Berat</button>
-        <button>Snack</button>
-        <button>Minuman</button>
+
+        <button onclick="filterMenu('all')">
+            Semua
+        </button>
+
+        <button onclick="filterMenu('makanan')">
+            Makanan Berat
+        </button>
+
+        <button onclick="filterMenu('snack')">
+            Snack
+        </button>
+
+        <button onclick="filterMenu('minuman')">
+            Minuman
+        </button>
+
     </div>
 
-    <!-- MAKANAN BERAT -->
-    <h3 class="judul-kategori">Makanan Berat</h3>
+    <!-- MAKANAN -->
+    <div class="kategori makanan">
 
-    <div class="menu-grid">
+        <h3 class="judul-kategori">
+            🍛 Makanan Berat
+        </h3>
 
-        <!-- 12 Menu Makanan -->
-        <?php
-        $makanan = [
-            ["Nasi Goreng","Rp20.000","img/nasigoreng.png"],
-            ["Bakso","Rp15.000","img/bakso.png"],
-            ["Mie Ayam","Rp15.000","img/mieayam.png"],
-            ["Nila Bakar","Rp25.000","img/nilabakar.jpg"],
-            ["Rawon","Rp25.000","img/rawon.jpg"],
-            ["Sate Lilit","Rp15.000","img/satelilit.jpg"],
-            ["Seafood Mix","Rp35.000","img/seafoodmix.jpg"],
-            ["Steak","Rp55.000","img/steak.jpg"],
-            ["Plecing Kangkung","Rp10.000","img/plecingkangkung.jpg"],
-            ["Ayam Betutu","Rp35.000","img/ayambetutu.jpg"],
-            ["Capcay","Rp20.000","img/capcay.jpg"],
-            ["Spagetti","Rp25.000","img/spagetti.jpg"]
-        ];
+        <div class="menu-grid">
 
-        foreach($makanan as $item){
-            echo "
-            <div class='menu-card'>
-                <img src='{$item[2]}' alt='{$item[0]}'>
-                <div class='menu-info'>
-                    <h4>{$item[0]}</h4>
-                    <p>{$item[1]}</p>
+            <?php foreach($makanan as $item){ ?>
+
+            <div class="menu-card">
+
+                <img src="<?= $item[2] ?>" alt="<?= $item[0] ?>">
+
+                <div class="menu-info">
+
+                    <h4><?= $item[0] ?></h4>
+
+                    <p>
+                        Rp <?= number_format($item[1],0,',','.') ?>
+                    </p>
+
+                    <form method="POST">
+
+                        <input type="hidden"
+                               name="id"
+                               value="<?= $item[0] ?>">
+
+                        <input type="hidden"
+                               name="nama"
+                               value="<?= $item[0] ?>">
+
+                        <input type="hidden"
+                               name="harga"
+                               value="<?= $item[1] ?>">
+
+                        <button
+                            type="submit"
+                            name="cart"
+                            class="btn-cart">
+
+                            🛒 Tambah Keranjang
+
+                        </button>
+
+                    </form>
+
                 </div>
-            </div>";
-        }
-        ?>
+
+            </div>
+
+            <?php } ?>
+
+        </div>
+
+    </div>
+
+    <!-- SNACK -->
+    <div class="kategori snack">
+
+        <h3 class="judul-kategori">
+            🍟 Snack
+        </h3>
+
+        <div class="menu-grid">
+
+            <?php foreach($snack as $item){ ?>
+
+            <div class="menu-card">
+
+                <img src="<?= $item[2] ?>" alt="<?= $item[0] ?>">
+
+                <div class="menu-info">
+
+                    <h4><?= $item[0] ?></h4>
+
+                    <p>
+                        Rp <?= number_format($item[1],0,',','.') ?>
+                    </p>
+
+                    <form method="POST">
+
+                        <input type="hidden"
+                               name="id"
+                               value="<?= $item[0] ?>">
+
+                        <input type="hidden"
+                               name="nama"
+                               value="<?= $item[0] ?>">
+
+                        <input type="hidden"
+                               name="harga"
+                               value="<?= $item[1] ?>">
+
+                        <button
+                            type="submit"
+                            name="cart"
+                            class="btn-cart">
+
+                            🛒 Tambah Keranjang
+
+                        </button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+            <?php } ?>
+
+        </div>
+
     </div>
 
     <!-- MINUMAN -->
-    <h3 class="judul-kategori">Minuman</h3>
+    <div class="kategori minuman">
 
-    <div class="menu-grid">
+        <h3 class="judul-kategori">
+            🥤 Minuman
+        </h3>
 
-        <?php
-       $minuman = [
-    ["Jus Alpukat","Rp12.000","img/jusalpukat.png"],
-    ["Kopi","Rp10.000","img/kopi.jpg"],
-    ["Boba Matcha Latte","Rp15.000","img/Matcha.Png"],
-    ["Boba Taro","Rp15.000","img/bobataro.jpg"],
-    ["Jus Mangga","Rp13.000","img/jusmangga.jpg"],
-    ["The Red Oriental","Rp17.000","img/redoriental.jpg"],
-    ["Es Teler","Rp15.000","img/esteler.png"],
-    ["Boba Brown Sugar","Rp15.000","img/brownsugar.jpg"],
-    ["Lemon Tea","Rp13.000","img/lemontea.jpg"],
-    ["Smoothies Strawberry","Rp16.000","img/smoothies.jpg"],
-    ["Choco Hazelnut Frappe","Rp20.000","img/chocohazelnut.jpg"],
-    ["Lychee White Blossom","Rp16.000","img/lychee.jpg"]
-];
+        <div class="menu-grid">
 
-        foreach($minuman as $item){
-            echo "
-            <div class='menu-card'>
-                <img src='{$item[2]}' alt='{$item[0]}'>
-                <div class='menu-info'>
-                    <h4>{$item[0]}</h4>
-                    <p>{$item[1]}</p>
+            <?php foreach($minuman as $item){ ?>
+
+            <div class="menu-card">
+
+                <img src="<?= $item[2] ?>" alt="<?= $item[0] ?>">
+
+                <div class="menu-info">
+
+                    <h4><?= $item[0] ?></h4>
+
+                    <p>
+                        Rp <?= number_format($item[1],0,',','.') ?>
+                    </p>
+
+                    <form method="POST">
+
+                        <input type="hidden"
+                               name="id"
+                               value="<?= $item[0] ?>">
+
+                        <input type="hidden"
+                               name="nama"
+                               value="<?= $item[0] ?>">
+
+                        <input type="hidden"
+                               name="harga"
+                               value="<?= $item[1] ?>">
+
+                        <button
+                            type="submit"
+                            name="cart"
+                            class="btn-cart">
+
+                            🛒 Tambah Keranjang
+
+                        </button>
+
+                    </form>
+
                 </div>
-            </div>";
-        }
-        ?>
-    </div>
 
-    <!-- DESSERT -->
-    <h3 class="judul-kategori">Dessert</h3>
+            </div>
 
-    <div class="menu-grid">
+            <?php } ?>
 
-        <?php
-        $dessert = [
-            ["Panna Cotta","Rp17.000","img/pannacotta.jpg"],
-            ["Strawberry Parfait","Rp15.000","img/parfait.jpg"],
-            ["Fried Ice Cream","Rp10.000","img/friedicecream.jpg"],
-            ["Semifreddo","Rp17.000","img/semifreddo.jpg"],
-            ["Pancake","Rp12.000","img/pancake.jpg"],
-            ["Risol Mayo","Rp15.000","img/Risol.png"],
-            ["French Fries","Rp12.000","img/kentang.png"],
-            ["Sandwich","Rp15.000","img/sandwich.jpg"],
-            ["Brownie Tiramisu","Rp14.000","img/brownietiramisu.jpg"],
-            ["Momo","Rp16.000","img/momo.jpg"],
-            ["Gyoza","Rp16.000","img/gyoza.jpg"],
-            ["Salted Caramel Ice Cream Squares","Rp15.000","img/saltedcaramel.jpg"]
-        ];
+        </div>
 
-        foreach($dessert as $item){
-            echo "
-            <div class='menu-card'>
-                <img src='{$item[2]}' alt='{$item[0]}'>
-                <div class='menu-info'>
-                    <h4>{$item[0]}</h4>
-                    <p>{$item[1]}</p>
-                </div>
-            </div>";
-        }
-        ?>
     </div>
 
 </section>
 
 <?php
-include 'includes/footer.php';
+include 'includes/footer.php';   
 ?>
-
-
-include 'includes/footer.php'
