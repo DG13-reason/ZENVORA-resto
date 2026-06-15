f2a1db9f4deee6092730b05485132cb852101353
DG13-Reason <damargiri13@gmail.com>
2026-06-15 11:59:55 +0800

    perubahan index fitur keranjang

--- index.php
+++ index.php
@@ -4,11 +4,44 @@ session_start();
 if(isset($_SESSION['user'])){
     echo "Selamat datang " . $_SESSION['user']['username'];
 }
+
+if(isset($_POST['cart'])){
+
+    $id = $_POST['id'];
+
+    if(isset($_SESSION['cart'][$id])){
+        $_SESSION['cart'][$id]['qty']++;
+    }else{
+        $_SESSION['cart'][$id] = [
+            'nama'  => $_POST['nama'],
+            'harga' => $_POST['harga'],
+            'qty'   => 1
+        ];
+    }
+
+    header("Location: cart.php");
+    exit;
+}
+
+include 'includes/header.php';
+include 'includes/navbar.php';
+
+$makanan = [
+    ["Nasi Goreng",20000,"assets/images/Menu/Nasgor.png"],
+    ["Bakso",15000,"assets/images/Menu/bakso.png"],
+    ["Mie Ayam",15000,"assets/images/Menu/MieAyam.png"]
+];
+
+$minuman = [
+    ["Jus Alpukat",12000,"assets/images/Menu/jusAlpukat.png"],
+    ["Kopi",10000,"assets/images/Menu/kopi.jpg"]
+];
+
+$snack = [
+    ["French Fries",12000,"assets/images/Menu/French Fries.jpg"],
+    ["Sandwich",15000,"assets/images/Menu/Sandwich.jpg"]
+];
 ?>
-
-<?php include 'includes/header.php'; ?>
-
-<?php include 'includes/navbar.php'; ?>
 
 
 <section class="search-sect">
@@ -59,107 +92,97 @@ if(isset($_SESSION['user'])){
                 Dessert & Snack
             </button>
         </div>
-
+        
         <div class="menu-container">
+            <?php foreach($makanan as $item){ ?>
             <div class="menu-card main">
                 <div class="menu-img">
-                    <img src="assets/images/Menu/Bakso.png" alt="">
+                    <img src="<?=  $item[2] ?>" alt="<?= $item[0] ?>">
                 </div>
                 <div class="menu-desc">
                     <div class="tittle">
-                        <h3>Bakso</h3>
+                        <h3><?= $item[0] ?></h3>
                         <div class="rating">
                             <i data-feather="star"></i>
                             <span>4.5</span>
                         </div>
                     </div>
-                    <p>Bakso dengan daging premium</p>
-                    <span>Rp25.000</span>
+                    <p>Rp <?= number_format($item[1],0,',','.') ?></p>
                 </div>
-                <div class="menu-footer">
-                    <button class="cart-btn">
+                <form method="POST">
+                    
+                    <div class="menu-footer">
+                    <button type="submit" name="cart"  class="cart-btn">
                         <i data-feather="shopping-cart"></i>
                     </button>
 
                     <button class="pesan-btn">pesan</button>
                 </div>
+                </form>
+                
             </div>
-            <div class="menu-card main">
-                <div class="menu-img">
-                    <img src="assets/images/Menu/Bakso.png" alt="">
-                </div>
-                <div class="menu-desc">
-                    <div class="tittle">
-                        <h3>Bakso</h3>
-                        <div class="rating">
-                            <i data-feather="star"></i>
-                            <span>4.5</span>
-                        </div>
-                    </div>
-                    <p>Bakso dengan daging premium</p>
-                    <span>Rp25.000</span>
-                </div>
-                <div class="menu-footer">
-                    <button class="cart-btn">
-                        <i data-feather="shopping-cart"></i>
-                    </button>
+            <?php } ?>
 
-                    <button class="pesan-btn">pesan</button>
-                </div>
-            </div>
-
+            <?php foreach($minuman as $item){ ?>
             <div class="menu-card drinks">
                 <div class="menu-img">
-                    <img src="assets/images/Menu/Bakso.png" alt="">
+                    <img src="<?=  $item[2] ?>" alt="<?= $item[0] ?>">
                 </div>
                 <div class="menu-desc">
                     <div class="tittle">
-                        <h3>Bakso</h3>
+                        <h3><?= $item[0] ?></h3>
                         <div class="rating">
                             <i data-feather="star"></i>
                             <span>4.5</span>
                         </div>
                     </div>
-                    <p>Bakso dengan daging premium</p>
-                    <span>Rp25.000</span>
+                    <p>Rp <?= number_format($item[1],0,',','.') ?></p>
                 </div>
-                <div class="menu-footer">
-                    <button class="cart-btn">
+                <form method="POST">
+
+                    <div class="menu-footer">
+                    <button type="submit" name="cart"  class="cart-btn">
                         <i data-feather="shopping-cart"></i>
                     </button>
 
                     <button class="pesan-btn">pesan</button>
                 </div>
+                </form>
+                
             </div>
+            <?php } ?>
 
+            <?php foreach($snack as $item){ ?>
             <div class="menu-card desserts">
                 <div class="menu-img">
-                    <img src="assets/images/Menu/Bakso.png" alt="">
+                    <img src="<?=  $item[2] ?>" alt="<?= $item[0] ?>">
                 </div>
                 <div class="menu-desc">
                     <div class="tittle">
-                        <h3>Bakso</h3>
+                        <h3><?= $item[0] ?></h3>
                         <div class="rating">
                             <i data-feather="star"></i>
                             <span>4.5</span>
                         </div>
                     </div>
-                    <p>Bakso dengan daging premium</p>
-                    <span>Rp25.000</span>
+                    <p>Rp <?= number_format($item[1],0,',','.') ?></p>
                 </div>
-                <div class="menu-footer">
-                    <button class="cart-btn">
+                <form method="POST">
+                    <div class="menu-footer">
+                    <button type="submit" name="cart"  class="cart-btn">
                         <i data-feather="shopping-cart"></i>
                     </button>
 
                     <button class="pesan-btn">pesan</button>
                 </div>
+                </form>
+                
             </div>
+            <?php } ?>
         </div>
-
         
     </div>
-
+    
 </section>
 
 
